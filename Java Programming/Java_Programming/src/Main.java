89576070a07c68a2da7a4cd1416f8aa057327333
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-01 23:44:38 +0530

    Adding comments

--- Java Programming/Java_Programming/src/Main.java
+++ Java Programming/Java_Programming/src/Main.java
@@ -1,6 +1,7 @@
-//Importing Java Util so we can get the input from user
+//My first Java program
+//Importing Java Util,so we can get the input from user
 import java.util.*;
-
+// Defining Nomenclature
 public class Main {
     public static void main(String[] args) {
 //        Scanner Function
@@ -8,8 +9,10 @@ public class Main {
        System.out.print("Hello World!\n");
 
        System.out.print("My Name Is Yogesh Jha \n");
+//       Taking Input from the user
         System.out.print("Enter your name \n");
         String userName = sc.nextLine();
+//        Printing the output which is given by the user
         System.out.print("Your name is " + userName);
 
     }
