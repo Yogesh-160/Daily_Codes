a5ecce8c98c5827e47e737834c9b0ca3b55c302c
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-01 23:20:59 +0530

    Updating Source code with Comments

--- Java Programming/Java_Programming/src/Main.java
+++ Java Programming/Java_Programming/src/Main.java
@@ -1,9 +1,12 @@
+//Importing Java Util so we can get the input from user
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+//        Scanner Function
        Scanner sc = new Scanner (System.in);
        System.out.print("Hello World!\n");
+
        System.out.print("My Name Is Yogesh Jha \n");
         System.out.print("Enter your name \n");
         String userName = sc.nextLine();
