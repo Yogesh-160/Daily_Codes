962883edab18cd9aa48427472d9df900af38d2d2
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-01 20:24:48 +0530

    Updating Source code

--- Java Programming/Java_Programming/src/Main.java
+++ Java Programming/Java_Programming/src/Main.java
@@ -1,6 +1,13 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+       Scanner sc = new Scanner (System.in);
        System.out.print("Hello World!\n");
-       System.out.print("My Name Is Yogesh Jha");
+       System.out.print("My Name Is Yogesh Jha \n");
+        System.out.print("Enter your name \n");
+        String userName = sc.nextLine();
+        System.out.print("Your name is " + userName);
+
     }
 }
