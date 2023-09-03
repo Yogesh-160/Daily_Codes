9cf29029e99cd41cf897346c08e6bd735689a61e
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-03 23:17:33 +0530

    Updating codes

--- Java Programming/Java_Programming/src/Main.java
+++ Java Programming/Java_Programming/src/Main.java
@@ -1,19 +1,8 @@
-//My first Java program
-//Importing Java Util,so we can get the input from user
-import java.util.*;
-// Defining Nomenclature
+//Simple Java program to print Hello world on screen or console
 public class Main {
     public static void main(String[] args) {
-//        Scanner Function
-       Scanner sc = new Scanner (System.in);
        System.out.println("Hello World! This is my First Java code\n");
 
-       System.out.println("My Name Is Yogesh Jha \n");
-//       Taking Input from the user
-        System.out.println("Enter your name \n");
-        String userName = sc.nextLine();
-//        Printing the output which is given by the user
-        System.out.println("Your name is " + userName);
 
     }
 }
