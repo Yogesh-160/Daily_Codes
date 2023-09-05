bce2824ac8708accd348420496ccda5b9e615caf
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-06 00:05:08 +0530

    Updating Javascript code

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,5 +2,15 @@
 // Started to learn Javascript from scratch
 // Date : 3 September 2023
 
-let a = "Hello Yogesh !";
-console.log(a);
+/*let a = "Hello Yogesh !";
+console.log(a);*/
+
+let a = prompt("Hello, What is your age?");
+a = Number.parseInt(a);
+if(a>0){
+  alert("This is the valid range");
+}
+else{
+  alert("This is not a valid range");
+}
+undefined
