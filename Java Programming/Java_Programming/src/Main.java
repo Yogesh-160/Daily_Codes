1fceebad850c2aedb8201ebeba1883749ceb359d
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-05 23:42:56 +0530

    Updating codes in Java

--- Java Programming/Java_Programming/src/Main.java
+++ Java Programming/Java_Programming/src/Main.java
@@ -1,8 +1,12 @@
 //Simple Java program to print Hello world on screen or console
 public class Main {
     public static void main(String[] args) {
-       System.out.println("Hello World! This is my First Java code\n");
-
-
+//       System.out.println("Hello World! This is my First Java code\n");
+//Program to add three numbers
+        int num1 = 6;
+        int num2 = 8;
+        int num3 = 9;
+        int Result = num1+num2+num3;
+        System.out.println("Num1 + Num2 + Num3 = " + Result);
     }
 }
