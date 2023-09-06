8d1230e244dbfb716c07bb520c318b7a2fb3b36b
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-07 00:54:24 +0530

    Updated Java code

--- Java Programming/Java_Programming/src/Literals_Java.java
+++ Java Programming/Java_Programming/src/Literals_Java.java
@@ -1,5 +1,8 @@
 public class Literals_Java {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        byte age = 20;
+        char ch = 'A';
+        float f1 = 5.6f;
+        System.out.println(age);
     }
 }
