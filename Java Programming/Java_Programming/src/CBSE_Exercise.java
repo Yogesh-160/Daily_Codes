17a1c5f512578eb3a473ea40bfa80fe69d622823
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-08 16:46:42 +0530

    Make code more usable and usable

--- Java Programming/Java_Programming/src/CBSE_Exercise.java
+++ Java Programming/Java_Programming/src/CBSE_Exercise.java
@@ -4,6 +4,18 @@ public class CBSE_Exercise {
         System.out.println("Java Program to calculate percentage of 5 Subjects");
         Scanner sc = new Scanner(System.in);
 //        We can give Scanner name sc or anything.
+        System.out.println("Max marks of Hindi = ");
+        int Hin = sc.nextInt();
+        System.out.println("Max marks of English = ");
+        int En = sc.nextInt();
+        System.out.println("Max marks of Maths = ");
+        int Ma = sc.nextInt();
+        System.out.println("Max marks of Social Science = ");
+        int SS = sc.nextInt();
+        System.out.println("Max marks of Science = ");
+        int Sci = sc.nextInt();
+        int Total_Marks = Hin+En+Ma+SS+Sci;
+
         System.out.println("Enter Marks of Hindi : ");
          float Hindi = sc.nextFloat();
         System.out.println("Enter Marks of English : ");
@@ -14,7 +26,9 @@ public class CBSE_Exercise {
         float Social = sc.nextFloat();
         System.out.println("Enter Marks of science");
         float science = sc.nextFloat();
-        float Result = ((Hindi+English+Maths+Social+science)*100)/600;
+
+
+        float Result = ((Hindi+English+Maths+Social+science)*100)/Total_Marks;
         System.out.println("The Percentage you achieved is = " + Result);
 
 
