3c1a0cdf52a34842d8285531f7c87dd8795cd634
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-08 15:31:10 +0530

    Updating Java code-Input from user

--- Java Programming/Java_Programming/src/Taking_Input_Java.java
+++ Java Programming/Java_Programming/src/Taking_Input_Java.java
@@ -1,5 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class Taking_Input_Java {
     public static void main(String[] args) {
-        System.out.println("It works");
+        System.out.println("Taking input from the user");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number 1  : ");
+        int a = sc.nextInt();
+        System.out.println("Enter Number 2  : ");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("The sum of these numbers is ");
+        System.out.println(sum);
     }
 }
