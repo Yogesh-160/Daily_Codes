03eb6c122d9756feb0e377c41b068027b223af46
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-09 22:49:21 +0530

    Updating code

--- C Programming/Program.c
+++ C Programming/Program.c
@@ -2,10 +2,18 @@
 
 #include<stdio.h>
 int main(){
-    int a;
-printf("Hello world! Yogesh Jha \n");
-printf("Enter number 1 : ");
-scanf("%d", &a);
-printf("You entered the number %d", a);
+    int a = 30;
+//     int a;
+// printf("Hello world! Yogesh Jha \n");
+// printf("Enter number 1 : ");
+// scanf("%d", &a);
+// printf("You entered the number %d", a);
+// If else loops
+if(a > 20){
+    printf("A is greater than 20");
+
+}
+else
+printf("A is smaller than 20");
 return 0;
 }
