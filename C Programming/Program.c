de93d812a0fbb0bc2ad366067ec7569bbbf99473
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-09 19:00:57 +0530

    Updating codes

--- C Programming/Program.c
+++ C Programming/Program.c
@@ -2,6 +2,10 @@
 
 #include<stdio.h>
 int main(){
-printf("Hello world!");
+    int a;
+printf("Hello world! Yogesh Jha \n");
+printf("Enter number 1 : ");
+scanf("%d", &a);
+printf("You entered the number %d", a);
 return 0;
 }
