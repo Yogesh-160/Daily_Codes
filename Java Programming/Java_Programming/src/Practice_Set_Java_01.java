c9fb6da4af684ddc1e390d8ee655b9364e4b4b45
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-12 21:26:27 +0530

    Adding sum three numbers in java code

---
+++ Java Programming/Java_Programming/src/Practice_Set_Java_01.java
@@ -0,0 +1,9 @@
+public class Practice_Set_Java_01 {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 10;
+        int c = 11;
+        int sum = a+b+c;
+        System.out.println("The sum of three numbers is = " + sum);
+    }
+}
