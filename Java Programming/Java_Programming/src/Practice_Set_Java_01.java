ea9e109ed4f0031f24c12f899c5a1e3c97c4760b
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-12 21:36:45 +0530

    Adding CGPA code

--- Java Programming/Java_Programming/src/Practice_Set_Java_01.java
+++ Java Programming/Java_Programming/src/Practice_Set_Java_01.java
@@ -1,9 +1,18 @@
 public class Practice_Set_Java_01 {
     public static void main(String[] args) {
-        int a = 9;
-        int b = 10;
-        int c = 11;
-        int sum = a+b+c;
-        System.out.println("The sum of three numbers is = " + sum);
+//      Q-1  Program to find Sum of three numbers
+//        int a = 9;
+//        int b = 10;
+//        int c = 11;
+//        int sum = a+b+c;
+//        System.out.println("The sum of three numbers is = " + sum);
+
+//        Q-2 Program to calculate CGPA of three subject out of 100
+        float Subject1 = 45;
+        float Subject2 = 50;
+        float Subject3 = 69;
+        float CGPA = (Subject1+Subject2+Subject3)/30; //We consider 30 except 3 because we have to find CGPA out of 10 and thus, 3 * 10 = 30
+        System.out.println(CGPA);
+
     }
 }
