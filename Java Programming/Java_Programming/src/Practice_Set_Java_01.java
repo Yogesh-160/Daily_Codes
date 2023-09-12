9df41cb7ff28266efc053a9e09ad851f3a26bd90
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-12 21:54:01 +0530

    Conversion of Km to mies

--- Java Programming/Java_Programming/src/Practice_Set_Java_01.java
+++ Java Programming/Java_Programming/src/Practice_Set_Java_01.java
@@ -21,5 +21,11 @@ public class Practice_Set_Java_01 {
         String name = sc.nextLine();
         System.out.println("Hello!" + name + ",have a good day.");*/
 
+//        Q-4 Program to convert kilometer value in miles
+        double km = 7;
+        double miles = km/1.6;
+        System.out.println("Miles = " + miles);
+
+
     }
 }
