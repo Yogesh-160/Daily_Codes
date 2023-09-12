586d8ae11f245e1dec3e9eac42968ee1286cbf51
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-12 21:49:30 +0530

    Adding Taking input in java code

--- Java Programming/Java_Programming/src/Practice_Set_Java_01.java
+++ Java Programming/Java_Programming/src/Practice_Set_Java_01.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Practice_Set_Java_01 {
     public static void main(String[] args) {
 //      Q-1  Program to find Sum of three numbers
@@ -8,11 +9,17 @@ public class Practice_Set_Java_01 {
 //        System.out.println("The sum of three numbers is = " + sum);
 
 //        Q-2 Program to calculate CGPA of three subject out of 100
-        float Subject1 = 45;
+        /*float Subject1 = 45;
         float Subject2 = 50;
         float Subject3 = 69;
         float CGPA = (Subject1+Subject2+Subject3)/30; //We consider 30 except 3 because we have to find CGPA out of 10 and thus, 3 * 10 = 30
-        System.out.println(CGPA);
+        System.out.println(CGPA);*/
+
+//        Q-3 Program to take user input and return wish with his or her name
+        /*System.out.println("What is your name? : ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello!" + name + ",have a good day.");*/
 
     }
 }
