18bbfbba7282547e6a720ba9f50ad6e66f00e31b
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-13 21:28:17 +0530

    Code update java

--- Java Programming/Java_Programming/src/Operators.java
+++ Java Programming/Java_Programming/src/Operators.java
@@ -2,7 +2,9 @@ public class Operators {
     public static void main(String[] args) {
         int a = 4;
         int b = 6%a ;
+        double c = 4.8%1.1;
         System.out.println(b);
+        System.out.println(c);
     }
 
 }
