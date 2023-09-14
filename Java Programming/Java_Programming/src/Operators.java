f0af709bb4ab7c804de1be90c77b27efc093c5a1
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-14 21:31:02 +0530

    Adding code

--- Java Programming/Java_Programming/src/Operators.java
+++ Java Programming/Java_Programming/src/Operators.java
@@ -3,10 +3,14 @@ public class Operators {
         int a = 4;
         int b = 6%a ;
         double c = 4.8%1.1;
+        int d = b+a;
+        int e = b-a;
         System.out.println(b);
         System.out.println(a);
         System.out.println(b);
         System.out.println(c+a);
+        System.out.println(d);
+        System.out.println(e);
     }
 
 }
