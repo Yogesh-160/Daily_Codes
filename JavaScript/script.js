db40e4ed321c8107d31988ad64b1a9c516f4508d
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-16 22:30:32 +0530

    Updating code

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,12 +5,15 @@
 /*let a = "Hello Yogesh !";
 console.log(a);*/
 
-let a = prompt("Hello, What is your age?");
+/*let a = prompt("Hello, What is your age?");
 a = Number.parseInt(a);
 if(a>0){
   alert("This is the valid range");
 }
 else{
   alert("This is not a valid range");
-}
-undefined
+}*/
+
+let name = "Yogesh";
+console.log(name.length);
+ 
