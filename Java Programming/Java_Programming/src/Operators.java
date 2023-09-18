5091bcb61c999353f16a342ba40712b0cc09464f
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-18 21:05:30 +0530

    Update

--- Java Programming/Java_Programming/src/Operators.java
+++ Java Programming/Java_Programming/src/Operators.java
@@ -5,11 +5,13 @@ public class Operators {
         int c = a+b;
         int d = a/b;
         int e = a%b;
+        int f = a-b;
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
         System.out.println(e);
+        System.out.println(f);
     }
 
 }
