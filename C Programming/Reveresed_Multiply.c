436cbb3eb0e3a632bb867910a0ded099874d8d88
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-21 20:00:46 +0530

    Updating code

--- C Programming/Reveresed_Multiply.c
+++ C Programming/Reveresed_Multiply.c
@@ -6,7 +6,7 @@ int main()
     printf("Program to execute reversed order multiplication of 10");
     for (int i = 11; i; i--)
     {
-        printf("10 X %d = %d\n", i, 10 * i);
+        printf(" 10 X %d = %d\n", i, 10 * i);
     }
     return 0;
 }
