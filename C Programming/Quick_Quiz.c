a9d94f6e249ea94ff2ab11200bb11bb6484e00b2
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-23 17:51:01 +0530

    Updating

--- C Programming/Quick_Quiz.c
+++ C Programming/Quick_Quiz.c
@@ -1,12 +1,17 @@
 // Write a program to print natural numbers from 10 to 20 when initial loop counter is initialized to 0
 #include<stdio.h>
 int main(){
-int a = 0;
+/*int a = 0;
 while(a<=20){
     if(a>=10){
         printf("The value of a is %d\n", a);
     }
     a++;
+}*/
+
+for(int i=0;i<=10;i++){
+    printf("%d",i);
+    printf("\n");
 }
 
 return 0;
