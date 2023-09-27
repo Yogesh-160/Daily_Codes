056940723bdc285947f09d3177ef7418e25cf445
Yogesh-160 <nityanandrekhajha@gmail.com>
2023-09-27 21:20:07 +0530

    Updating

---
+++ Java Programming/Java_Programming/src/classT.java
@@ -0,0 +1,14 @@
+
+    class T {
+        int t = 20;
+        T() {
+            t = 40;
+        }
+    }
+    class classT {
+        public static void main(String args[]) {
+            T t1 = new T();
+            System.out.println(t1.t);
+        }
+    }
+
